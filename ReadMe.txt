7ffb7d7ca9c42fc022fbcd9ad971780bac527f00
Diego Chacón <dchacon130@gmail.com>
2022-06-16 16:00:08 -0500

    new ReadMe comment

---
+++ ReadMe.txt
@@ -0,0 +1,3 @@
+
+Login example:
+https://platzi.com/tutoriales/1378-python/1406-creando-registro-de-usuario-e-inicio-de-sesion-con-django/?utm_source=google&utm_medium=cpc&utm_campaign=17418244234&utm_adgroup=&utm_content=&gclid=Cj0KCQjwhqaVBhCxARIsAHK1tiM66k-TwncqHVEw188ieZL1MoPot38PQHB0i6Fbt9NlNyyYz_idiN4aAghJEALw_wcB&gclsrc=aw.ds
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+
+from django.views.generic import CreateView, TemplateView
+
+from .models import Profile
+
+from .form import SingUpForm
 
 # Create your views here.
+class SingUpView(CreateView):
+    model = Profile
+    form_class = SingUpForm
+
+    def form_class(self, form):
